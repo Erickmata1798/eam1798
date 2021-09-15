b8bae6e510709f84d00ed3fdbc8e64f4a02da82b
unknown <emataq17@hotmail.com>
2021-09-15 10:13:52 -0600

    SERVICES FIXED

--- src/components/Services.js
+++ src/components/Services.js
@@ -9,7 +9,16 @@ const Services = () => {
             <div className="services__accordion">
                 <Accordion defaultActiveKey="0">
                     <Accordion.Item eventKey="0">
-                        <Accordion.Header>Accordion Item #1</Accordion.Header>
+                        <Accordion.Header>Sed ut perspiciatis</Accordion.Header>
+                        <Accordion.Body>
+                        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy<br/> 
+                        eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam<br/>
+                         voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita<br/> 
+                         kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+                        </Accordion.Body>
+                    </Accordion.Item>
+                    <Accordion.Item eventKey="1">
+                        <Accordion.Header>Ut enim ad minima veniam</Accordion.Header>
                         <Accordion.Body>
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
                             tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
@@ -20,8 +29,8 @@ const Services = () => {
                             est laborum.
                         </Accordion.Body>
                     </Accordion.Item>
-                    <Accordion.Item eventKey="1">
-                        <Accordion.Header>Accordion Item #2</Accordion.Header>
+                    <Accordion.Item eventKey="2">
+                        <Accordion.Header>Sit amet, consectetur, adipisci</Accordion.Header>
                         <Accordion.Body>
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
                             tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
