9cca977ae2c97badf61dc833d6a6395cc68820ba
unknown <emataq17@hotmail.com>
2021-09-15 10:08:07 -0600

    Services FIXED

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,54 +1,40 @@
 import React from 'react';
 import imagen from '../images/section4-image.png';
-import { Accordion, Card } from "react-bootstrap";
-import "bootstrap/dist/css/bootstrap.min.css";
+import { Accordion} from 'react-bootstrap';
+
 const Services = () => {
     return (
         <div className="Services__container">
             <img src={imagen} alt="Imagen" className="services__imagen"></img>
             <div className="services__accordion">
-                <Accordion>
-                    <Card>
-                        <Accordion.Toggle as={Card.Header} eventKey="0">
-                            Sed ut perspiciatis
-                        </Accordion.Toggle>
-
-                        <Accordion.Collapse eventKey="0">
-                            <Card.Body>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy<br />
-                                eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam<br />
-                                voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita<br />
-                                kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</Card.Body>
-                        </Accordion.Collapse>
-                    </Card>
-
-                    <Card>
-                        <Accordion.Toggle as={Card.Header} eventKey="1">
-                            Ut enim ad minima veniam
-                        </Accordion.Toggle>
-
-                        <Accordion.Collapse eventKey="1">
-                            <Card.Body>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy<br />
-                                eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam<br />
-                                voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita<br />
-                                kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</Card.Body>
-                        </Accordion.Collapse>
-                    </Card>
-                    <Card>
-                        <Accordion.Toggle as={Card.Header} eventKey="2">
-                        Sit amet, consectetur, adipisci
-                        </Accordion.Toggle>
-
-                        <Accordion.Collapse eventKey="2">
-                            <Card.Body>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy<br />
-                                eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam<br />
-                                voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita<br />
-                                kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</Card.Body>
-                        </Accordion.Collapse>
-                    </Card>
-
+                <Accordion defaultActiveKey="0">
+                    <Accordion.Item eventKey="0">
+                        <Accordion.Header>Accordion Item #1</Accordion.Header>
+                        <Accordion.Body>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+                            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                            commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+                            velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                            cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+                            est laborum.
+                        </Accordion.Body>
+                    </Accordion.Item>
+                    <Accordion.Item eventKey="1">
+                        <Accordion.Header>Accordion Item #2</Accordion.Header>
+                        <Accordion.Body>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+                            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                            commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+                            velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                            cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+                            est laborum.
+                        </Accordion.Body>
+                    </Accordion.Item>
                 </Accordion>
             </div>
-            
+
         </div>
 
     );
